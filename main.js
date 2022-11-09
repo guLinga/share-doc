423193edc80f74433f7f92fe94441fab1e7c0b65
qq84808045 <2634917964@qq.com>
2022-11-09 14:43:09 +0800

    feat:添加上线配置，用来打包

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const isDev = require('electron-is-dev');
+const path = require('path');
 const { app, BrowserWindow } = require('electron');
 require('@electron/remote/main').initialize()
+
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({
@@ -15,6 +17,6 @@ app.on('ready', () => {
   })
   require('@electron/remote/main').enable(mainWindow.webContents)
   mainWindow.webContents.openDevTools();
-  const urlLocation = isDev ? 'http://localhost:3000' : '';
+  const urlLocation = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname,'./build/index.html')}`;
   mainWindow.loadURL(urlLocation);
 })

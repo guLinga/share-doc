0d351102592207a53e037cbe0013e90a9b233540
qq84808045 <2634917964@qq.com>
2022-11-09 16:41:10 +0800

    feate:阿里图标库组件

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,7 @@
+import { createFromIconfontCN } from '@ant-design/icons';
+
+const IconFont = createFromIconfontCN({
+	scriptUrl: '//at.alicdn.com/t/c/font_3760306_mqm6ole1vw8.js',
+});
+
+export default IconFont;

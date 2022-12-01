d89d0b59dd2cf15224fbaecc2ff20f62827bfc86
qq84808045 <2634917964@qq.com>
2022-12-01 15:45:20 +0800

    feat:请求日历列表，将列表传入日历组件

--- src/pages/dairy/index.tsx
+++ src/pages/dairy/index.tsx
@@ -1,14 +1,28 @@
 import './index.scss';
 import Calendar from '../../components/Calendar/index';
 import DiaryContent from '../../components/diaryContent/index';
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
+import axios from '../../utils/axios';
 
 export default function Test() {
+
+  //日历组件返回选中的日期
   const [date,setDate] = useState('');
+
+  // 日期列表
+  const [dateList,setDateList] = useState(null);
+
+  useEffect(()=>{
+    (async function fn(){
+      let result = await axios({url: '/diary/calendar'});
+      if(result.data.data)setDateList(result.data.data);
+    })()
+  },[])
+
   return (
     <div className='dairy'>
       <div className='calendarVessels'>
-        <Calendar width="100%" dayCilck={(day)=>{
+        <Calendar width="100%" dataList={dateList} dayCilck={(day)=>{
           setDate(day);
         }}/>
       </div>

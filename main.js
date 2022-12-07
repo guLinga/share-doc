7d2e9ebb307fc7845e755c10d260f51fc3efec0c
qq84808045 <2634917964@qq.com>
2022-12-07 20:03:00 +0800

    feat:将开启窗口设置成两个，使得可以测试发送消息

--- main.js
+++ main.js
@@ -20,10 +20,26 @@ app.on('ready', () => {
       contextIsolation: false
     }
   })
+  const secondWindow = new BrowserWindow({
+    width: 1024,
+    height: 680,
+    autoHideMenuBar: true,
+    // frame: false,
+    // titleBarStyle: 'hidden',
+    webPreferences: {
+      preload: "./preload.js",
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      contextIsolation: false
+    }
+  })
   require('@electron/remote/main').enable(mainWindow.webContents)
+  require('@electron/remote/main').enable(secondWindow.webContents)
   if(isDev){
     mainWindow.webContents.openDevTools();
+    secondWindow.webContents.openDevTools();
   }
   const urlLocation = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname,'./build/index.html')}`;
   mainWindow.loadURL(urlLocation);
+  secondWindow.loadURL(urlLocation);
 })

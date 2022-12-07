c945bde7172bc79f37fb8906821fb11884bd7896
qq84808045 <2634917964@qq.com>
2022-12-07 21:34:43 +0800

    feat:修改笔友聊天页面左侧布局样式

---
+++ src/pages/friends/component.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled"
+import { Button } from "antd"
+
+export const RightBtn = styled(Button)`
+  position: absolute;
+  bottom: 25px;
+  right: 0;
+  height: 35px;
+  padding: 0 20px;
+`
--- src/pages/friends/index.tsx
+++ src/pages/friends/index.tsx
@@ -2,19 +2,29 @@ import { Resizable } from 're-resizable';
 import './index.scss';
 import FriendsListVessles from '../../components/friendsListVessels/index';
 import {props} from './type'
-import { useState, useEffect, createContext } from 'react';
+import { useState, useEffect, createContext, useRef } from 'react';
+import {Input} from 'antd';
+import { RightBtn } from './component';
+import { TextAreaRef, TextAreaProps } from 'antd/es/input/TextArea';
+
+const { TextArea } = Input;
 
 export const UserIdContext = createContext<{selectUserId:number|undefined,setSelectUserId:React.Dispatch<React.SetStateAction<number | undefined>>}|undefined>(undefined);
 
 function Friends({socket,userId}:props) {
 
   // 输入框
-  const [val,setVal] = useState('');
+  const inputEl = useRef(null);
 
   const [selectUserId,setSelectUserId] = useState<number|undefined>(undefined);
 
   // 发送消息
   const send = () => {
+    //@ts-ignore
+    const val = inputEl.current.resizableTextArea.textArea.value;
+    //@ts-ignore
+    console.log(String(inputEl.current.resizableTextArea.textArea.value));
+    
     socket.current?.emit("send-msg",{
       to: selectUserId,
       from: userId,
@@ -25,8 +35,8 @@ function Friends({socket,userId}:props) {
   // 接收消息
   useEffect(()=>{
     if (socket.current) {
-      socket.current.on("msg-recieve", (msg) => {
-        console.log('msg-recieve-msg:',msg);
+      socket.current.on("msg-recieve", (msg:string) => {
+        console.log('msg-recieve-msg:',msg,msg.replace('\n','<br>'));
       });
     }
   },[socket.current])
@@ -52,8 +62,14 @@ function Friends({socket,userId}:props) {
             selectUserId!==undefined && 
             <>
               userId:{userId},selectUserId:{selectUserId}
-              <input type="text" value={val} onChange={(e)=>{setVal(e.target.value)}} />
-              <button onClick={send}>发送</button>
+              <div className='chat'>
+              <div className='header'>昵称</div>
+                <div className='message white-space:pre'></div>
+                <div className='send'>
+                  <TextArea ref={inputEl} style={{height:'100%'}} rows={4}/>
+                  <RightBtn type='primary' onClick={send}>发送</RightBtn>
+                </div>
+              </div>
             </>
           }
         </div>

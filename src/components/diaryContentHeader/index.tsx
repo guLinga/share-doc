00b6f5aefa4aa74112b13d831bb0c4be7e20f17b
GuLing <2634917964@qq.com>
2023-04-15 15:30:09 +0800

    style：去除多余的日记功能

--- src/components/diaryContentHeader/index.tsx
+++ src/components/diaryContentHeader/index.tsx
@@ -2,7 +2,8 @@ import './index.scss'
 import {useState} from 'react';
 import { diaryContentHeaderProps } from './type';
 export default function DiaryContentHeader({listText,setListText}:diaryContentHeaderProps) {
-  const list = ["日记","附件","音频","视频","聊天"];
+  // const list = ["日记","附件","音频","视频","聊天"];
+  const list = ["日记","音频"];
   return (
     <div className="diaryContentHeader">
       {
